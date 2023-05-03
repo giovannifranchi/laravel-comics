23f817bba9a9ebbf487cfac8b7fb6d7d074dd706
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-03 16:09:29 +0200

    Add: header-top completed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,21 @@
 <header>
-    header
+    <div class="header-top">
+        <div class="container">
+            <div class="d-flex justify-content-end gap-5 align-items-center">
+                <h6 class="m-0">DC POWER VISA</h6>
+                <!-- Dropdown -->
+                <div class="btn-group">
+                    <button class="btn btn-sm dropdown-toggle p-0 fs-6 font-family-inherit text-reset border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      ADDITIONAL DC SITES
+                    </button>
+                    <ul class="dropdown-menu ps-3">
+                      <li>one option</li>
+                      <li>two option</li>
+                      <li>three option</li>
+                    </ul>
+                </div>
+                <!-- Dropdown -->
+            </div>
+        </div>
+    </div>
 </header>
